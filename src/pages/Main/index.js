faf7289420586df9a72b56c2ed63865d1dbb13de
gumiranda <imartytk@hotmail.com>
2019-10-06 19:50:50 -0300

    ajustes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,71 +1,111 @@
-import React,{Component} from 'react';
-import { Title } from './styles';
-import {FaGithubAlt,FaPlus,FaSpinner} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
-import {Container} from '../../components/container';
-import{Form,SubmitButton,List} from './styles';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
+
 export default class Main extends Component {
-    state = {
-        newRepo:'',
-        repositories:[],
-        loading:false,
-    };
-    componentDidMount(){
-        const repositories = localStorage.getItem('repositories');
-        if(repositories){
-            this.setState({repositories:JSON.parse(repositories)});
-        }
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: null,
+  };
+
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
     }
-   componentDidUpdate(_,prevState){
-       const {repositories} = this.state;
-       if(prevState.repositories !== repositories){
-           localStorage.setItem('repositories',JSON.stringify(repositories));
-       }
-   }
-   handleInputChange = e =>{
-       this.setState({newRepo:e.target.value});
-   };
-   handleSubmit = async = e =>{
-       e.preventDefault();
-       this.setState({loading:true});
-       const {newRepo,repositories} = this.state;
-       const response = await api.get(`/repos/${newRepo}`);
-       const data = {
-           name:response.data.full_name
-       };
-       this.setState({
-           repositories:[...repositories,data],
-           newRepo:'',
-           loading:false
-       });
-   };
+  }
 
-render(){
-    return (<Container>
-    <h1>
-       <FaGithubAlt/>
-       Repositories of people
-    </h1>
-    <Form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Adicionar repositório" value={newRepo} onChange={this.handleInputChange}/>
-<SubmitButton loading={loading}>
-    {loading ?(<FaSpinner color="#fff" size={14}/>) : (<FaPlus color="#fff" size={14}/>) }
-</SubmitButton>
-    </Form>
-    <List>
-        {repositories.map(repository =>(
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value, error: null });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true, error: false });
+
+    try {
+      const { newRepo, repositories } = this.state;
+
+      if (newRepo === '') throw 'Você precisa indicar um repositório';
+
+      const hasRepo = repositories.find(r => r.name === newRepo);
+
+      if (hasRepo) throw 'Repositório duplicado';
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, repositories, loading, error } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
             <li key={repository.name}>
-                <span>
-                    {repository.name}
-                </span>
-                <Link to={`/repository/${`/repository/${encodeURIComponent(repository.name)}`}`}>
-                    Detalhes
-                </Link>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
-        ))}
-    </List>
-    </Container>);
-}
-
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Main from './pages/Main';
 import Repository from './pages/Repository';
+
 export default function Routes() {
-    return (
-        <BrowserRouter>
-        <Switch>
-            <Route path="/repository" component={Repository} />
-            <Route path="/" component={Main} />
-        </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
