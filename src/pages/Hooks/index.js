de206a893ee570097a226cc61fb24ad5f4977768
Gustavo Miranda <gustavoa@kyros.com.br>
2019-10-07 16:33:53 -0300

    hook useMemo

--- src/pages/Hooks/index.js
+++ src/pages/Hooks/index.js
@@ -1,43 +1,39 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
+export default function Hooks() {
+    const [tech, setTech] = useState([]);
+    const [newTech, setNewTech] = useState('');
 
-export default function Hooks()  {
-const [tech,setTech] = useState([]);
-const [newTech,setNewTech] = useState('');
+    function handleAdd() {
+        setTech([...tech, newTech]);
+        setNewTech('');
+    }
 
-function handleAdd(){
-    setTech([...tech,newTech]);
-    setNewTech('');
+    useEffect(() => {
+        const storageTech = localStorage.getItem('tech');
+        if (storageTech) {
+            setTech(JSON.parse(storageTech));
+        }
+
+        // return () => {}; //event listener - componentwilldidunmount
+        // document.removeEventListener()
+    }, []);
+    useEffect(() => {
+        localStorage.setItem('tech', JSON.stringify(tech));
+    }, [tech]);
+    const techSize = useMemo(() => tech.length, [tech]);
+    return (
+        <>
+            <ul>
+                {tech.map(t => (
+                    <li key={t}>{t}</li>
+                ))}
+            </ul>
+            <strong>Você tem {techSize} tecnologias</strong>
+            <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+            <button type="button" onClick={handleAdd}>
+                Add
+            </button>
+        </>
+    );
 }
-
-useEffect(()=>{
-const storageTech = localStorage.getItem('tech');
-if(storageTech){
-    setTech(JSON.parse(storageTech));
-}
-
-//return () => {}; //event listener - componentwilldidunmount
-//document.removeEventListener()
-},[]);
-useEffect(()=>{
-localStorage.setItem('tech',JSON.stringify(tech));
-},[tech]);
-
-return (
-    <>
-    <ul>
-        {tech.map(t=>(
-            <li key={t}>{t}</li>
-        ))}
-    </ul>
-    <input value={newTech} onChange={e=>setNewTech(e.target.value)}/>
-    <button type="button" onClick={handleAdd}>
-    Add
-    </button>
-    </>
-);
-
-
-}
-
-
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ import Main from './pages/Main';
 import Repository from './pages/Repository';
 
 export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Hooks} />
-        <Route path="/main" component={Main} />
-        <Route path="/repository/:repository" component={Repository} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Hooks} />
+                <Route path="/main" component={Main} />
+                <Route path="/repository/:repository" component={Repository} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
