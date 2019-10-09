7d22dc4da18435240609578eb16e53292938d0ae
Gustavo Miranda <gustavoa@kyros.com.br>
2019-10-09 15:11:03 -0300

    tela signin estilizada

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
-
 export default function SignIn() {
-    return <h1>Login</h1>;
+    return (
+        <>
+            <img src={logo} alt="Barber" />
+            <form>
+                <input type="email" placeholder="Seu email" />
+                <input type="password" placeholder="Sua senha" />
+                <button type="submit">Acessar</button>
+                <Link to="/register">
+                    Ainda não tem conta? Clique aqui para criar
+                </Link>
+            </form>
+        </>
+    );
 }
