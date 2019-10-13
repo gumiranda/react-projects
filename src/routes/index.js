986c88b43d65e63f3724f955746e8b4dc4217906
gumiranda <imartytk@hotmail.com>
2019-10-13 11:22:44 -0300

    ajustando rotas para quando entrar em produção estar ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,17 +9,34 @@ import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 
-
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/dashboard" component={Dashboard} isPrivate />
-                <Route path="/register" component={SignUp} />
-                <Route path="/" exact component={SignIn} />
-                <Route path="/profile" component={Profile} isPrivate />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/dashboard`}
+                    component={Dashboard}
+                    isPrivate
+                />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/register`}
+                    component={SignUp}
+                />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    exact
+                    component={SignIn}
+                />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/profile`}
+                    component={Profile}
+                    isPrivate
+                />
 
-                <Route path="/" component={()=> <h1>404</h1>} />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    component={() => <h1>404</h1>}
+                />
             </Switch>
         </BrowserRouter>
     );
