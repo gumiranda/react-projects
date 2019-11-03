0dda0645a41b1d7703d8bf5d5d8e3347ab2886b1
gumiranda <imartytk@hotmail.com>
2019-11-03 12:29:10 -0300

    começando a criar pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return (
-    <div />
-  );
+    const profile = useSelector(state => state.user.profile);
+    function handleSubmit(data) {}
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu endereço de email"
+                />
+                <hr />
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Sua senha"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirme a nova senha"
+                />
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+            <button type="button">Sair do Barber</button>
+        </Container>
+    );
 }
